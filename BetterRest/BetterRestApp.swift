754b154d8c35d83e820185767235530028ccffc4
ysmaraujo <yasminnaraujo@hotmail.com>
2024-06-12 23:39:34 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Yasmin araujo on 12/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
